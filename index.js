40add58cfc633ccb43d45717269e1620b3d0cdfd
Rob Richard <rob@1stdibs.com>
2015-10-22 11:02:01 -0400

    can lint staged git files

--- index.js
+++ index.js
@@ -1,50 +1,17 @@
-require('es6-promise').polyfill();
-
+var lintFileGroup = require('./lib/lintFileGroup');
+var lintGitFiles = require('./lib/lintGitFiles');
 var opt = require('node-getopt').create([
     ['w', 'warnings', 'enable warnings'],
     ['e', 'es6', 'allow es6 in .js files'],
+    ['', 'git', 'lint only staged git files'],
     ['', 'root[=path]', 'look for js files in this directory'],
     ['h', 'help', 'display this help']
 ])
 .bindHelp()
 .parseSystem();
 
-var path = require('path');
-var glob = require('glob');
-var fs = require('fs');
-var getLinter = require('./getLinter');
-var promiseChild = require('./promiseChild');
-var reactLinter = getLinter(path.resolve(__dirname, './configs/react.js'), opt.options);
-var es6Linter = getLinter(path.resolve(__dirname, './configs/es6.js'), opt.options);
-var es5Linter = getLinter(path.resolve(__dirname, './configs/es5.js'), opt.options);
-var root = opt.options.root || '.';
-var files;
-
-if (opt.argv.length) {
-    files = opt.argv;
+if (opt.options.git) {
+    process.exit(lintGitFiles(opt));
 } else {
-    files = glob.sync(root + '/**/*.@(js|jsx)');
+    process.exit(lintFileGroup(opt));
 }
-
-files.forEach(function (filePath) {
-    if (filePath.match(/.jsx$/)) {
-        reactLinter.stdin.write(filePath + '\0');
-    } else if (filePath.match(/.es.js$/) || (opt.options.es6 && filePath.match(/.js$/))) {
-        es6Linter.stdin.write(filePath + '\0');
-    } else if (filePath.match(/.js$/)) {
-        es5Linter.stdin.write(filePath + '\0');
-    }
-});
-reactLinter.stdin.end();
-es6Linter.stdin.end();
-es5Linter.stdin.end();
-
-Promise.all([
-    promiseChild(reactLinter),
-    promiseChild(es6Linter),
-    promiseChild(es5Linter)
-]).then(function (args) {
-    if (!(args[0] === args[1] === args[2] === 0)) {
-        process.exit(1);
-    }
-});
--- promiseChild.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (child) {
-	return new Promise(function (resolve, reject) {
-		child.on('close', function(code) {
-			resolve(code);
-		});
-	});
-};
