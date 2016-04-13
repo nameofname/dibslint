53691f44e7a1d71cf77a2c5eccfab4a2e5a137ab
Carter McKendry <carter.mckendry@gmail.com>
2016-04-13 19:57:14 -0400

    Support additional eslint argument for pass-through, es6 changes (#13)

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 #!/usr/bin/env node
+const spawnSync = require('child_process').spawnSync;
 
-var lintFileGroup = require('./lib/lintFileGroup');
-var lintGitFiles = require('./lib/lintGitFiles');
-var opt = require('node-getopt').create([
+const lintFileGroup = require('./lib/lintFileGroup');
+const lintGitFiles = require('./lib/lintGitFiles');
+const opt = require('node-getopt').create([
     ['w', 'warnings', 'enable warnings'],
     ['e', 'es6', 'allow es6 in .js files'],
     ['', 'git', 'lint only staged git files'],
     ['', 'root[=path]', 'look for js files in this directory'],
-    ['h', 'help', 'display this help']
+    ['h', 'help', 'display this help'],
+    ['f', 'format=format', 'set output format'],
+    ['v', 'version', 'display eslint version info']
 ])
 .bindHelp()
 .parseSystem();
 
-if (opt.options.git) {
+if (opt.options.version) {
+    const esLintVer = spawnSync('eslint', ['-v']);
+    process.stdout.write(esLintVer.stdout);
+    process.exit();
+} else if (opt.options.git) {
     process.exit(lintGitFiles(opt));
 } else {
     process.exit(lintFileGroup(opt));
